a98a6d98ce93d65da9444a7968c3b3065e9efb53
raka-arrayan <rakaarrayan27@gmail.com>
2025-05-04 11:30:08 +0700

    merubah index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); 
 const dotenv = require('dotenv');
 const routes = require('./routes/contactRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
+
+app.use(cors());            
 app.use(express.json());  
 
-// Gunakan semua route dari folder routes (prefix: /api)
-app.use('/api', routes);
+app.use('/api', routes);   
+app.use(errorHandler);       
 
-// Middleware untuk menangani error
-app.use(errorHandler);
-
-// Jalankan server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
